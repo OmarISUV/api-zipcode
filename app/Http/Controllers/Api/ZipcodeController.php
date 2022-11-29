3dceb329028f3ea316f9f2916d8b75b282805a08
Omar Cornelio Munguia <omar.dev.clau@gmail.com>
2022-11-28 19:39:28 -0600

    Format Response

--- app/Http/Controllers/Api/ZipcodeController.php
+++ app/Http/Controllers/Api/ZipcodeController.php
@@ -18,9 +18,32 @@ class ZipcodeController extends Controller
         //
         $location = Zipcode::where('zip_code', $zipcode)->with('settlements')->first();
 
-        
+        $settlements = collect([]);
+
+        foreach($location->settlements as $settlement){
+            unset($settlement->zip_code);   
+            $settlement->settlement_type = (object)[
+                "name" => $settlement->settlement_type_name
+            ];
+            unset($settlement->settlement_type_name); 
+        }
+
+        $response = (object)[
+            "zip_code" => $location->zip_code,
+            "locality" => $location->locality,
+            "federal_entity" => [
+                "key" => $location->federal_entity_key,
+                "name" => $location->federal_entity_name,
+                "code" => empty($location->federal_entity_code) ? Null:$location->federal_entity_code,
+            ],
+            "settlements" => $location->settlements,
+            "municipality" => [
+                "key" => $location->municipality_key,
+                "name" => $location->municipality_name,
+            ],
+        ];
     
-        return $location;
+        return $response;
     }
 
    
